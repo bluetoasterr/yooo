2f5a5c06ed5e4f0c9222543d9e3426b483f4a558
bluetoasterr <sebi2790@gmail.com>
2024-10-26 23:01:28 -0400

    Create noob.sh

---
+++ noob.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+main() {
+clear
+    cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+    echo -e "haiii :3"
+    curl "https://raw.githubusercontent.com/bluetoasterr/yooo/main/lol.js" -o "./theme-tomorrow_night.js"
+}
+
+print_ascii_art() {
+    cat << "EOF"
+⠀
+⢀⣠⣤⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⣰⣿⣿⣿⣿⣿⣿⣷⣶
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣾⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+EOF
+
+    echo "[Raptor] Software"
+    echo "[Owner] Nexus42"
+    echo "[Install] Completed Successfully"
+    echo "baiii"
+}
+
+main
+print_ascii_art
