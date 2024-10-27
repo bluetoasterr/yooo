269999ded9411642a394c951419be1ad86dfb851
bluetoasterr <sebi2790@gmail.com>
2024-10-26 23:40:51 -0400

    flop.sh

---
+++ flop.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+main() {
+clear
+    cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+    echo -e "echo playback starting now floppa theme"
+    curl "https://raw.githubusercontent.com/bluetoasterr/yooo/main/floopa.js" -o "./theme-tomorrow_night.js"
+}
+
+print_ascii_art() {
+    cat << "EOF"
+⠀
+⢀⣠⣤⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⣰⣿⣿⣿⣿⣿⣿⣷⣶
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣾⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+EOF
+
+    echo "[Raptor] Software"
+    echo "[Owner] Nexus42"
+    echo "[Install] Completed Successfully"
+    echo "baiii"
+}
+
+main
+print_ascii_art
